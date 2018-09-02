92563fdf61d9fe72ee0653815a32039006aa6d42
Ruargh <ruargh@gmail.com>
2018-09-02 04:12:40 +0000

    Create static version

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import logo from './logo.svg';
+import MovieList from'./MovieList'
 
 /*
 Use React and the data below to display a list of users alongside their favorite movies.
@@ -106,7 +107,7 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <h2>Favorite Movies</h2>
-		
+        <MovieList />
       </div>
     );
   }
---
+++ src/MovieList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MovieList extends Component {
+  render() {
+    return(
+      <ol>
+        <li>Jane Cruz''s favorite movie is Planet Earth 1.</li>
+      </ol>
+      )
+  }
+}
+
+export default MovieList
